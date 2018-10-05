c89723d8aaebe7a4b36208649d5cb21a5018846b
evgenkarp01 <ewal@list.ru>
2018-10-05 12:45:25 +0000

    05102018

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+	$(".slider-slick-banner").slick({
+		prevArrow: $('.prev-arrow-banner'),
+		nextArrow: $('.next-arrow-banner'),
+		autoplay: true,
+		speed: 3400,
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		dotsClass: 'slider__dots',
+	})
+
+	$(".services-arrow").on("click", function(){
+		$(this).parent(".service").toggleClass("active");
+		$(this).siblings(".oplet").toggle('fast');
+		$(this).children("img").toggleClass("services-arrow-scale");
+	});
+
+
+})
+
+
