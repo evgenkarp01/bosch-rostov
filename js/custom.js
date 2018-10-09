a935bd310b57de217962ba148e51b77624ad665e
evgenkarp01 <ewal@list.ru>
2018-10-09 12:46:35 +0000

    finish

--- js/custom.js
+++ js/custom.js
@@ -102,6 +102,47 @@ $( document ).ready(function() {
         $('[name="phone_footer"]').mask("+7(999) 999-99-99");
         $('[name="phone"]').mask("+7(999) 999-99-99");
     });
+
+
+
+
+    function call(ev) {
+ 	  var msg   = $(ev).closest('form').serialize();
+        $.ajax({
+          type: 'POST',
+          url: '../php/send.php',
+          data: msg,
+          success: function(data) {
+            $(ev).closest('.result').html(data);
+            $(ev).slideUp('fast');
+          },
+          error:  function(xhr, str){
+	    alert('Возникла ошибка: ' + xhr.responseCode);
+          }
+        });
+    }
+    
+    
+    $("#contacts-button").on("click", function(event){
+        event.preventDefault();
+         
+         if(($('[name="phone_footer"]').val()!='')&&($('[name="phone_footer"]').val()!='+7(___)___-__-__')){
+            call(this);
+         }
+    });
+    
+    $("#modal-button").on("click", function(event){
+        event.preventDefault();
+         
+         if(($('[name="phone"]').val()!='')&&($('[name="phone"]').val()!='+7(___)___-__-__')){
+            call(this);
+         }
+    });
+
+
+
+
+
+
+
 })
-
-
